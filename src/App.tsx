61d01ddc0ea67f576db3c4760356e74966a1256d
Shashank Sreepathi <sreepsha1234@gmail.com>
2025-05-29 00:44:48 -0700

    Added Homepage

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './Components/home'
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
